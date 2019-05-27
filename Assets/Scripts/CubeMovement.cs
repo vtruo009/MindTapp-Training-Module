9e90ea42ba9d3d1c7f4a5d5e3c463b59469349ab
Van Truong <vtruo009@ucr.edu>
2019-05-26 18:43:14 -0700

    Cube movement done

---
+++ Assets/Scripts/CubeMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMovement : MonoBehaviour
+{
+    void FixedUpdate() {
+        //input
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            Vector3 position = this.transform.position;
+            position.x = position.x-.06F;
+            if (position.x > -2.3) {
+                this.transform.position = position;
+            }
+        }
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            Vector3 position = this.transform.position;
+            // position.x++;
+            position.x = position.x+.06F;
+            if (position.x < 2.3) {
+                this.transform.position = position;
+            }
+        }
+    }
+}
